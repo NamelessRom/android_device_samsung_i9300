af61eb920f0576f803dbaa20b8719000ade39fe8
elelinux <ele.linux@gmail.com>
2014-01-09 22:09:08 +0100

    i9300: Update build fingerprint
    Change-Id: Ia70712ed166015187bcf9f7d830e61c9a57f0511

--- nameless_i9300.mk
+++ nameless_i9300.mk
@@ -21,6 +21,13 @@
 # lines, full and toro, hence its name.
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := i9300
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/nameless/config/apns.mk)
 
@@ -42,3 +49,6 @@ PRODUCT_MODEL := GT-I9300
 
 # Inherit the SIM Toolkit
 PRODUCT_PACKAGES += Stk
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
