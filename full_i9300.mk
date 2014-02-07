b91983d4991f77095cc6dc9522233f8a07a91be1
Jake Whatley <jake@nameless-rom.org>
2014-02-07 02:06:15 -0500

    move to new vendor config
    Change-Id: I9661bb57e07b943c894291f23ae7605cf47c3dd9

--- full_i9300.mk
+++ full_i9300.mk
@@ -21,10 +21,7 @@
 # lines, full and toro, hence its name.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
+#$(call inherit-product )
 $(call inherit-product, device/samsung/i9300/i9300.mk)
 
 # Discard inherited values and use our own instead.
