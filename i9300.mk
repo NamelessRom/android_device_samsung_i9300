e48bcf6321a2c2ac103e11359223a790447e1e57
Kilian von Pflugk <kilianvonpflugk@gmail.com>
2014-08-31 23:06:48 +0200

    Revert "use own blobs"
    This reverts commit 551ef70314f1e2b818b0d47e43db39829d3e554c.
    
    Change-Id: Id4a38a1539fdac88f981bd99d56b83cb292e256f

--- i9300.mk
+++ i9300.mk
@@ -97,4 +97,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-$(call inherit-product-if-exists, vendor/samsung_own/i9300/i9300-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/i9300/i9300-vendor.mk)
