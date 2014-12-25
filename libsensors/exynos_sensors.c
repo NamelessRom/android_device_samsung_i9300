90f77771ce66d784698fa74354f68a81d312fdd1
jumoog <kilianvonpflugk@gmail.com>
2014-12-25 13:34:05 +0100

    major sensor improvements
    from :https://gitorious.org/replicant/device_samsung_i9300/
    
    modified to work with L
    
    Change-Id: Id3a9f559bd90035595e7bb46b0d26093478ee2ae

--- libsensors/exynos_sensors.c
+++ libsensors/exynos_sensors.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Paul Kocialkowski
+ * Copyright (C) 2013 Paul Kocialkowski <contact@paulk.fr>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,13 +21,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <poll.h>
-
-
-
 #include <sys/select.h>
-
-
-
 #include <hardware/sensors.h>
 #include <hardware/hardware.h>
 
@@ -76,14 +70,14 @@ struct exynos_sensors_handlers *exynos_sensors_handlers[] = {
 	&lps331ap,
 };
 
-int exynos_sensors_handlers_count = sizeof(exynos_sensors_handlers) /
-	sizeof(struct exynos_sensors_handlers *);
+int exynos_sensors_handlers_count = sizeof(exynos_sensors_handlers) / sizeof(struct exynos_sensors_handlers *);
 
 /*
- * Exynos Sensors
+ * EXYNOS Sensors
  */
 
-int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle, int enabled)
+int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle,
+	int enabled)
 {
 	struct exynos_sensors_device *device;
 	int i;
@@ -122,7 +116,8 @@ int exynos_sensors_activate(struct sensors_poll_device_t *dev, int handle, int e
 	return -1;
 }
 
-int exynos_sensors_set_delay(struct sensors_poll_device_t *dev, int handle, int64_t ns)
+int exynos_sensors_set_delay(struct sensors_poll_device_t *dev, int handle,
+	int64_t ns)
 {
 	struct exynos_sensors_device *device;
 	int i;
@@ -295,7 +290,7 @@ struct sensors_module_t HAL_MODULE_INFO_SYM = {
 		.version_major = 1,
 		.version_minor = 0,
 		.id = SENSORS_HARDWARE_MODULE_ID,
-		.name = "Exynos Sensors",
+		.name = "EXYNOS Sensors",
 		.author = "Paul Kocialkowski",
 		.methods = &exynos_sensors_module_methods,
 	},
