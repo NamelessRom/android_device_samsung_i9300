a9106c213cd77493690aa3272a8d44ea6e21e1a0
Dheeraj CVR <cvr.dheeraj@gmail.com>
2016-03-24 12:18:20 +0400

    i9300: sensors: return the actual poll failure errno
    Change-Id: I268c28c1f2aeea43f99472f6db69f66ce27bbeb7

--- libsensors/smdk4x12_sensors.c
+++ libsensors/smdk4x12_sensors.c
@@ -164,7 +164,7 @@ int smdk4x12_sensors_poll(struct sensors_poll_device_t *dev,
 	do {
 		poll_rc = poll(device->poll_fds, device->poll_fds_count, n > 0 ? 0 : -1);
 		if (poll_rc < 0)
-			return -1;
+			return -errno;
 
 		for (i = 0; i < device->poll_fds_count; i++) {
 			if (!(device->poll_fds[i].revents & POLLIN))
